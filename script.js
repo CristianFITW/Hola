a6ed9cbdf35b63b666a911cb2d87a3003a7be8d8
unknown <gutierrez.flores.cristian.2006@gmail.com>
2024-09-18 20:52:03 -0600

    Script

---
+++ script.js
@@ -0,0 +1,40 @@
+alert("Wazaaaa");
+//Comentar codigo
+
+/*
+Comentar codigo con diversas lineas
+*/
+
+//18 de septiembre del 2024, soy Cristian Gutiérrez Flores Del 5IV9
+
+//Variables
+/*
+Las variables guardan información
+que se puede reutilizar
+*/
+
+
+//Declaracion de variables
+
+let nombre = "Cristian";
+const PI = 3.1415;
+var edad = 18;
+
+
+/*Tipos de datos
+Cadena de texto  (string): "Hola", 'Mundo'
+Numero  (numbres): 5, 13, 16
+Boleanos (boolean): true, false
+*/
+let mensaje = "Hola Ramirez";
+let numero = 42;
+let estudiante = true;
+
+//operadores aritmaticos
+let a=2;
+let b=5;
+let suma = a+b;
+let resta=a-b;
+let multiplicacion=a*b;
+let division=a/b;
+
