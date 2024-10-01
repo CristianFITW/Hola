9f96d55a2c3db0d553389d07ee82ec857ee33c6a
CristianFITW <gutierrez.flores.cristian.2006@gmail.com>
2024-09-30 20:15:12 -0600

    Wiki

---
+++ Clase - 230924/3.1UsoDeApiwikiPedia.js
@@ -0,0 +1,34 @@
+const terminoBusqueda = 'Programación'; // Término que deseas buscar 
+const url = `https://es.wikipedia.org/w/api.php?action=query&list=search&srsearch=${terminoBusqueda} &format=json&origin=*`; 
+
+
+async function buscarEnWikipedia () { 
+try { 
+const respuesta = await fetch (url); 
+const datos = await respuesta.json(); 
+const resultados datos.query.search; 
+if (resultados.length > 0) { 
+document.getElementById('resultados').textContent = `Resultados de búsqueda para "${terminoBusqueda}":`; 
+const listaResultados = document.getElementById('listaResultados'); 
+// Recorrer los resultados y crear una lista de artículos 
+resultados.forEach (resultado => { 
+const li = document.createElement('li'); 
+li.innerHTML = `<a 
+href="https://es.wikipedia.org/wiki/${resultado.title}" 
+target="_blank">${resultado.title} </a> 
+— 
+- 
+${resultado.snippet}`; 
+listaResultados.appendChild(li); 
+}); 
+} else { 
+document.getElementById('resultados').textContent = 'No seencontraron resultados. '; 
+} 
+} catch (error) { 
+// En caso de error, mostrar un mensaje 
+'No se 
+document.getElementById('resultados').textContent = 'Error al obtener los datos de Wikipedia.'; 
+} 
+} 
+console.error ('Error:', error); 
+// Llamar a la función para hacer la búsqueda cuando la página se carga buscarEnWikipedia (); 
