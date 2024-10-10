6ac51f9e2ef98ab6a4a8cfaefa7560c1f3cb30eb
unknown <gutierrez.flores.cristian.2006@gmail.com>
2024-10-09 20:46:02 -0600

    Documentado calculadora

--- U2_07102024/script.js
+++ U2_07102024/script.js
@@ -1,19 +1,25 @@
+//Funcion que añade un termino después de otro en el display
 function appendToDisplay (value) {
     document.getElementById('display').value += value;
 }
 
+//Funcion que limpia el display poniendo valores predeterminaods como "", practicamente el valor se cambia a un valor que es un espacio en blanco
 function clearDisplay(){
     document.getElementById('display'). value = '';
 }
+
+//Funcion que elimima el ultimo termino de una cadena de terminos del display, ejemplo, se tiene en el display 911 y se quita el ultimo termino que sería el 1
 function deleteLast(){
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
+//Calcula resultados de acuerdo ingresadi de la calculadora
 function calculateResult(){
     try{
+        //Realiza la operacion matematica y la despliega en el display
         let result = eval(document.getElementById('display').value);
         document.getElementById('display').value = result;
-    } catch (error){
-        alert('Expresión inválida');
+    } catch (error) {
+        //Si se ingresan terminos que hacen que no se pueda efectuar una operación, te marca un error (como el sintaxis error)
 }
 }
