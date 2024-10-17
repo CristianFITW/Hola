7505cdebb8b28926d76f957c69a3e2df9f69cbbd
unknown <gutierrez.flores.cristian.2006@gmail.com>
2024-10-16 20:41:43 -0600

    16_10_2024

---
+++ clase_16_10_2024/script.js
@@ -0,0 +1,6 @@
+//console.log("Hola");
+const h1 = document.createElement("h1");
+h1.createElement  = "Hola, clase del 16 de octubre";
+//console.log(h1.textContent);
+
+document.body.appendChild(h1);
